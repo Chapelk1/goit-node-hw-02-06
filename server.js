3e227297d542a3d2d6d02606af67f555fd4005a4
Chapelk1 <klepach154789@gmail.com>
2023-11-29 23:46:56 +0200

    +

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const app = require('./app')
+const mongoose = require('mongoose')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-});
+const DB_HOST =
+  "mongodb+srv://Anatolii:60XgXHt6xnLDgPjw@cluster0.d3d0all.mongodb.net/db_contacts?retryWrites=true&w=majority";
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  })
 
 
-`60XgXHt6xnLDgPjw`
+
